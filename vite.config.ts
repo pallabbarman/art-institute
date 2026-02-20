a617e621cb7514918e331f0d2c205ed74aaa4031
Pallab Bharman <bharmanpallab@gmail.com>
2026-02-20 23:38:06 +0600

    init project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest/config" />
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+        tailwindcss(),
+        tsconfigPaths(),
+    ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test_utils/setupTest.ts',
+    },
+});
