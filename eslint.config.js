dc2b2d50a48eae4107d1b8dbfd1216e6bc0760df
Pallab Bharman <bharmanpallab@gmail.com>
2026-03-12 17:53:04 +0600

    vscode settings added

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import storybook from 'eslint-plugin-storybook';
 import js from '@eslint/js';
 import pluginQuery from '@tanstack/eslint-plugin-query';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
